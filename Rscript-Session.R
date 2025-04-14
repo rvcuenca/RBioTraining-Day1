fe0c82bda637a620c889d5a3a3d0ab587ac3fb75
rvcuenca <rey.cuenca@g.msuiit.edu.ph>
2025-04-14 11:19:12 +0800

    updated

---
+++ Rscript-Session.R
@@ -0,0 +1,42 @@
+1 + 1
+sqrt(4)
+c(1,2,3,5,8,10.1)
+sqrt(c(1,2,3,5,8,10.1))
+
+c(1,2,3,5,8,10.1) + 1
+
+mean(c(1,2,3,5,8,10.1))
+sd(c(1,2,3,5,8,10.1))
+
+x <- c(1,2,3,5,8,10.1)
+
+x
+sqrt(x)
+x + 1
+x^2 + 1
+x^2 - 1
+(x - 1)*(x+1)
+log(x)
+log10(x)
+
+
+exp(-1.713)
+
+
+c(1,2,3,5,8,10.1) -> x_ni_rey
+c(1,2,3,5,8,10.1) -> x_ni_rey
+
+"I am Rey. Nice to meet you. April 14, 2025 $15.00"
+
+'I am Rey. Nice to meet you. April 14, 2025 $15.00'
+
+'I am Rey. "Nice to meet you". April 14, 2025 $15.00'
+
+"I am Rey. \"Nice to meet you\". April 14, 2025 $15.00"
+
+
+"I am Rey. Nice to meet you. April 14, 2025 $15.00"
+
+typeof("I am Rey. Nice to meet you. April 14, 2025 $15.00")
+
+
